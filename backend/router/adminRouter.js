c5bc9186736e4d896db164c8a8b9134f961304ae
Andre Putera Pratama <andreputerap@gmail.com>
2022-02-03 23:27:41 +0700

    Update adminRouter.js

--- backend/router/adminRouter.js
+++ backend/router/adminRouter.js
@@ -4,10 +4,14 @@ const { adminController } = require('../controller')
 const router = express.Router()
 
 router.get('/getAllUser', adminController.getAllUser)
+router.get('/getUserById', adminController.getUserById)
 router.post('/createUser', adminController.createUser)
 router.post('/toogleBanUser', adminController.toogleBanUser)
 router.post('/createAssignment', adminController.createAssignment)
 router.post('/toogleAssignment', adminController.toogleAssignment)
 router.get('/getAllAssignment', adminController.getAllAssignment)
+router.get('/getAssignmentById', adminController.getAssignmentById)
+router.get('/getUserAttendance', adminController.getUserAttendance)
+router.get('/getAttendanceById', adminController.getAttendanceById)
 
 module.exports = router
